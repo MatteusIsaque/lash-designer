6c91bb68a62470149ea296436e43da9de709887c
Matteus Isaque <matteus.isaque28@gmail.com>
2022-04-02 11:48:06 -0300

    ok

--- src/component/whasapp.tsx
+++ src/component/whasapp.tsx
@@ -8,7 +8,7 @@ import Link from 'next/link'
 export default function WhatsApp() {
 
   return (
-    <div id="buttonZap" className={styles.main}>
+    <div id="zapFlutuante" className={styles.main}>
       <Link href='https://api.whatsapp.com/send?phone=556199492015&text='>
         <Image src={Zap} />
       </Link>
